cd95fb11a0cc5a6eed924556a0a3b4691a13b343
SamratPandey <samratpandeyofficial02@gmail.com>
2024-05-27 21:43:33 +0530

    Password generator file update and learn about useEffect

--- 02bgChange/src/App.jsx
+++ 02bgChange/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 
 
 function App() {
-  const[color, setColor] = useState("black");
+  const[color, setColor] = useState("olive");
   return (
     <>
       <div className='w-full h-screen duration-200' style={{backgroundColor:color}}>
