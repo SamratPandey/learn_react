bf627211494c4c7edbf243a711813961a108126f
SamratPandey <samratpandeyofficial02@gmail.com>
2025-04-19 00:23:14 +0530

    Update changes

--- 05Todo/backEnd/config.js
+++ 05Todo/backEnd/config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () =>{
     try {
-        const response = await mongoose.connect('mongodb+srv://noName:7o6awyT7nU0WQN72@cluster1.6y9a2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1', {dbName:'todo-app'});
+        const response = await mongoose.connect(process.env.MONGO_URL, {dbName:'todo-app'});
         if(response){
             console.log("Database connected")
         }
